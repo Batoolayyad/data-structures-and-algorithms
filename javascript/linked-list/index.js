5121781a7418c179a7785e66b60da65d68fb0a22
Batoolayyad <batoolayyad1996@gmail.com>
2021-07-01 20:28:26 +0300

    linked list code challenge

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,56 @@
 'use strict';
 
+class Node {
+    constructor(value, next = null) {
+        this.value = value;
+        this.next = next;
+    }
+}
 class LinkedList {
-
+    constructor() {
+        this.head = null;
+    }
+    insert(value) {
+        let node = new Node(value);
+        if (!this.head) {
+            this.head = node;
+        } else {
+            node.next = this.head;
+            this.head = node;
+        }
+    }
+    includes(value) {
+        let current = this.head;
+        if (!this.head) {
+            return false;
+        }
+        while (current.next) {
+            if (current.value == value) {
+                return true;
+            }
+            current = current.next;
+            if ((current.next == null) && (value == current.value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    toString() {
+        let result = '';
+        let current = this.head;
+        while (current.next) {
+            if (current.value == null) {
+                result = result + `NULL -> `;
+            } else {
+                result = result + `{${current.value}} -> `;
+            }
+            current = current.next;
+            if (current.next == null) {
+                result = result + `{${current.value}}`;
+            }
+        }
+        return result;
+    }
 }
 
 module.exports = LinkedList;
